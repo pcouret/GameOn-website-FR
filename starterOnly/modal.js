734db58ed53a7662aedadcfbb52c087b2105acc1
COURET <philippe.couret@expleogroup.com>
2021-06-25 12:43:03 +0200

    maj fichiers

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,13 +7,19 @@ function editNav() {
   }
 }
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const close = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+close.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
