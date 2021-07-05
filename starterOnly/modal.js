1928b2681ab57b99eb48ea2a5ba52099348ca80e
COURET <philippe.couret@expleogroup.com>
2021-07-05 14:45:54 +0200

    maj fichiers 050721

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -27,3 +27,69 @@ function launchModal() {
 }
 
 
+function validate(event) {
+  event.preventDefault();
+  const first = document.getElementById("first").value;
+  console.log(first);
+  if (first.length < 2) {
+    console.log("Veuillez entrer votre Prenom")
+
+  } else {
+    console.log("Prénom OK")
+
+  }
+
+
+
+  const last = document.getElementById("last").value;
+  console.log(last);
+  if (last.length < 2) {
+    console.log("Veuillez entrer votre nom");
+  } else {
+    console.log("Nom OK")
+  }
+  const email = document.getElementById("email").value;
+  console.log(email);
+  if (last.length < 2) {
+    console.log("Veuillez entrer votre adresse email");
+  } else {
+    console.log("email OK")
+  }
+  const birthdate = document.getElementById("birthdate").value;
+  console.log(birthdate);
+  if (last.length < 2) {
+    console.log("Veuillez entrer votre date de naissance");
+  } else {
+    console.log("birthdate OK")
+  }
+  const quantity = document.getElementById("quantity").value;
+  console.log(quantity);
+  if (last.length < 2) {
+    console.log("Dans combien de tournois avez vous dejà participé");
+  } else {
+    console.log("Nombre de tournois OK")
+  }
+
+
+  const location1 = document.getElementById("location1").value;
+  console.log(location1);
+  if (document.getElementById("location1").checked == true) {
+    console.log("Choix ville OK");
+  } else {
+    console.log("Veuillez selectionner les villes. ")
+
+  }
+  const checkbox1 = document.getElementById("checkbox1").value;
+  console.log(checkbox1);
+  if (document.getElementById("checkbox1").checked == true) {
+    console.log("Acceptation OK");
+  } else {
+    console.log("Veuillez lire et accepter les conditions d'utilisation. ")
+
+  }
+}
+
+
+
+
+
