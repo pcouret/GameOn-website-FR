49ec105390579da0b14f37f4fd75677b86b1b80e
COURET <philippe.couret@expleogroup.com>
2021-07-13 13:57:34 +0200

    maj fichiers 13072021

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,18 +7,19 @@ function editNav() {
   }
 }
 
-function closeModal() {
-  modalbg.style.display = "none";
-}
+
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelectorAll(".close");
+const form = document.getElementById("form");
+const succes = document.getElementById("succes");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// close modal
 close.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
@@ -26,13 +27,22 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+/**
+ * 
+ */
+function closeModal() {
+  modalbg.style.display = "none";
+  form.style.display = "block";
+    succes.style.display = "none";
+}
 
 function validate(event) {
   event.preventDefault();
-  eventError = false;
+  let eventError = false;
 
   const first = document.getElementById("first").value;
   if (first.length < 2) {
+    eventError = true;
     firstError.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du prenom";
   } else {
     firstError.innerText = "";
@@ -40,40 +50,39 @@ function validate(event) {
 
   const last = document.getElementById("last").value;
   if (last.length < 2) {
-
+    eventError = true;
     lastError.innerText = "Veuillez entrer 2 caractères ou plus pour le champ du nom";
   } else {
     lastError.innerText = "";
   }
   const email = document.getElementById("email").value;
   if (email.length < 2) {
+    eventError = true;
     emailError.innerText = "Veuillez entrer votre adresse email";
   } else {
     emailError.innerText = "";
   }
   const birthdate = document.getElementById("birthdate").value;
   if (birthdate.length < 2) {
+    eventError = true;
     birthdateError.innerText = "Veuillez entrer votre date de naissance";
   } else {
     birthdateError.innerText = "";
   }
   const quantity = document.getElementById("quantity").value;
 
-  if (isNaN(quantity)) {
+  if (isNaN(quantity) || quantity<0 || quantity>99 ) {
+    eventError = true;
     quantityError.innerText = "Veuillez indiquer le nombre de tournois";
   } else {
     quantityError.innerText = "";
   }
-  if (quantity.length === 0) {
-    quantityError.innerText = "Veuillez indiquer le nombre de tournois";
-  } else {
-    quantityError.innerText = "";
-  }
+  
   if (checkLocation()) {
     location1Error.innerText = "";
   } else {
     location1Error.innerText = "Veuillez selectionner les villes.";
-
+    eventError = true;
   }
 
 
@@ -83,7 +92,7 @@ function validate(event) {
     checkbox1Error.innerText = "";
   } else {
     checkbox1Error.innerText = "Veuillez lire et accepter les conditions d'utilisation.";
-
+    eventError = true;
   }
 
 
@@ -91,7 +100,9 @@ function validate(event) {
     goError.innerHTML = "Inscription refusée";
     return false;
   } else {
-    alert('Inscription validée');
+    form.style.display = "none";
+    succes.style.display = "block";
+    form.reset();
   }
 }
 
@@ -109,9 +120,13 @@ function checkLocation() {
 
 }
 
-
+/**
+ * Verifiaction de l'email par rapport à la regex
+ * @param {*} email
+ * @returns boolean 
+ */
 function validateEmail(email) {
-  const emailReg = new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)[(a-zA-Z]{2,4})/i);
+  const emailReg = new RegExp(/^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i);
   const valid = emailReg.test(email);
 
   if ((!valid)) {
